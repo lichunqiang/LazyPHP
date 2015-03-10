e0181dfd5cb0bda19a39789fa4261e0a75041c92
lichunqiang <light-li@hotmail.com>
2015-03-10 22:49:17 +0800

    initialize

---
+++ config/app.config.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['config']['site_name'] = 'LazyPHP3';
+$GLOBALS['config']['site_domain'] = 'lazyphp3.sinaapp.com';
+$GLOBALS['config']['site_icon'] = image('lazyphplogowithoutname.128.png');
+
+$GLOBALS['config']['assets_version'] = '20150102';
